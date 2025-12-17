2661f5b6b5d6accc8eb7d53f88e1b1fdf4148095
manthewise <wisboyakins@gmail.com>
2025-12-17 13:54:32 +0100

    adjust startup message for logging feature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-console.log("Cloud Zero Day, starting...")
+console.log("cloud zero is a scam.")
 function startApp(){ const startTime = new Date();
 	console.log("Cloud Zero Day, starting...");
 	console.log("startup time:",startTime);}
