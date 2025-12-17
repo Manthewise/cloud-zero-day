441dbebd55e4674a6c1a944d9e9610cc55e71d1f
manthewise <wisboyakins@gmail.com>
2025-12-17 13:16:43 +0100

    sec js function test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
- Function startApp() {console.log("Cloud Zero Day, starting...") } 
+console.log("Cloud Zero Day, starting...")
+ Function startApp(){ const startTime = new Date(); console.log("Cloud Zero Day, starting..."); console.log("startup time:",startTime);
