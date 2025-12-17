618a5c569612c779017f9d4a6a255bb2410deb25
manthewise <wisboyakins@gmail.com>
2025-12-17 13:29:55 +0100

    second js test

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 console.log("Cloud Zero Day, starting...")
- Function startApp(){ const startTime = new Date(); console.log("Cloud Zero Day, starting..."); console.log("startup time:",startTime);
+function startApp(){ const startTime = new Date();
+	console.log("Cloud Zero Day, starting...");
+	console.log("startup time:",startTime);}
+startApp();
