ec1e617e7c21ddcb160600ce351ed11dc3a0b77d
Wisdom <111494836+Manthewise@users.noreply.github.com>
2025-12-18 15:13:52 +0100

    Update GH app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+console.log("change from github")
 console.log("cloud zero is a starting, hold on...")
 function startApp(){ const startTime = new Date();
 	console.log("Cloud Zero Day, starting...");
