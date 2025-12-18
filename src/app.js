ffdba318d7624759addb2c3d73bdb7d80f9b3d43
Wisdom <111494836+Manthewise@users.noreply.github.com>
2025-12-18 15:26:28 +0100

    Update rebase2 app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-console.log("change from github")
-console.log("cloud zero is a starting, hold on...")
+console.log("added another line");
+console.log("change from github");
+console.log("cloud zero is a starting, hold on...");
 function startApp(){ const startTime = new Date();
 	console.log("Cloud Zero Day, starting...");
 	console.log("startup time:",startTime);}
